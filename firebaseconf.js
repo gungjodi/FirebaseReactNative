61725017405923df4e7b4e053f8d39b570238ad3
Gung Jodi <jodipratama63@gmail.com>
2017-12-24 02:11:04 +0700

    add firebaseconf

---
+++ firebaseconf.js
@@ -0,0 +1,8 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "<your-api-key>",
+    databaseURL: "<your-database-url>",
+    storageBucket: "<your-storage-bucket-url>"
+  };
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
