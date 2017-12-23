4de06ca80bd683503efa221d8fe762a53bc4074d
Gung Jodi <jodipratama63@gmail.com>
2017-12-24 02:06:29 +0700

    Second commit

--- firebaseconf.js
+++
@@ -1,9 +0,0 @@
-
-import * as firebase from 'firebase';
-
-const firebaseConfig = {
-    apiKey: "<your-api-key>",
-    databaseURL: "<your-database-url>",
-    storageBucket: "<your-storage-bucket-url>"
-  };
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
