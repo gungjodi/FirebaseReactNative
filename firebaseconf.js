97fc09ca9cdac85f437064bc145264fa2ac8d251
Gung Jodi <jodipratama63@gmail.com>
2017-12-24 02:05:16 +0700

    Edit firebaseconf

--- firebaseconf.js
+++ firebaseconf.js
@@ -2,8 +2,8 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQuHWwK0AtshkERf56fBHfQ3tYa5BglLQ",
-    databaseURL: "gs://myproject2-a7edd.appspot.com",
-    storageBucket: "myproject2-a7edd.appspot.com"
+    apiKey: "<your-api-key>",
+    databaseURL: "<your-database-url>",
+    storageBucket: "<your-storage-bucket-url>"
   };
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
